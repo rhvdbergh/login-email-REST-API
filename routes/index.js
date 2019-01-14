d824a465b1689438ec57f369eb7367e69c078b89
Ronald van der Bergh <rhvdbergh@gmail.com>
2019-01-13 21:24:48 -0800

    basic user auth

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.post('/create-user', function(req, res, next) {
   if (req.body.email &&
     req.body.password &&
     req.body.passwordConf) {
-      console.log('request received');
+      console.log('create user account request received');
     var userData = {
       email: req.body.email,
       password: req.body.password,
@@ -32,6 +32,38 @@ router.post('/create-user', function(req, res, next) {
       }
     });
   } 
+})
+
+/* GET login user page. */
+router.get('/login-user', function(req, res, next) {
+  res.render('loginUser', {});
+})
+
+/* POST create user */
+router.post('/login-user', function(req, res, next) {
+  if (req.body.email &&
+    req.body.password) {
+      console.log('login user account request received');
+      User.authenticate(req.body.email, req.body.password, function(error, user) {
+        if (error || !user) {
+          var err = new Error('Wrong email or password.');
+          err.status = 401;
+          return next(err);
+        } else {
+          req.session.userId = user._id;
+          return res.redirect('/success');
+        }
+      }) // end user User.authenticate
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+})
+
+/* GET success for login */
+router.get('/success', function(req, res, next) {
+  res.render('success', {});
 })
 
 module.exports = router;
