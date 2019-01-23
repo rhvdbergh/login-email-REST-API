6a9835cd6c9d166765fe40385aa8a152f9f7ee0b
Ronald van der Bergh <rhvdbergh@gmail.com>
2019-01-22 22:01:01 -0800

    clean code, const var for easier reuse

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,23 +1,28 @@
 "use strict";
 const nodemailer = require("nodemailer");
 
+// var to set for code reuse 
+const SENDER_EMAIL_ADDRESS = '"Signie" <signupfrustration@gmail.com>';
+const HOST = 'smtp.gmail.com';
+const PORT = 465; 
+
 // async..await is not allowed in global scope, must use a wrapper
 async function sendMail(userName, userEmail, subject, message){
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
+    host: HOST,
+    port: PORT,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: process.env.EMAIL_ACCOUNT, // generated ethereal user
-      pass: process.env.EMAIL_PASS // generated ethereal password
+      user: process.env.EMAIL_ACCOUNT, 
+      pass: process.env.EMAIL_PASS 
     }
   });
 
-  // setup email data with unicode symbols
+  // setup email data - could include unicode symbols
   let mailOptions = {
-    from: '"Signie" <signupfrustration@gmail.com>', // sender address
+    from: SENDER_EMAIL_ADDRESS, // sender address
     to: userEmail, // list of receivers
     subject: subject, // Subject line
     text: message, // plain text body
@@ -28,13 +33,6 @@ async function sendMail(userName, userEmail, subject, message){
   let info = await transporter.sendMail(mailOptions)
 
   console.log("Message sent: %s", info.messageId);
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
-
-// main().catch(console.error);
 
 module.exports.sendMail = sendMail;
